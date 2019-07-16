6f789bfc9764deb8ff19466166f349869ad565ec
Laura Radville <laura.radville@gmail.com>
2019-07-16 18:32:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 require "pry"
 
 def key_for_min_value(name_hash)
-  smallest = name_hash.keys.sample
+  smallest = 10000000
   small_name = nil
   name_hash.each do |key, value|
     if value < smallest
